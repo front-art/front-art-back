c1f72c05aa119fb7b56bd3102f3ab4923e07bf68
ablfaxl <taghavi.a2023@gmail.com>
2024-05-24 17:57:38 +0330

    feat: update route

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ app.use(cors(corsOptions)); // Use the cors middleware
 
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
+app.use("/api/", authRoutes);
 
 // Set up Swagger
 setupSwagger(app);
